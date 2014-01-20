fcd3f96e5fd7def0eaf65f86f46a90d4d83dd5cb
Nicolas Froidure <nicolas_froidure@elitwork.com>
2014-01-20 02:02:43 +0100

    Trying to fix the publish

--- gulpfile.js
+++ gulpfile.js
@@ -16,10 +16,11 @@ require('matchdep').filterDev('gulp-*').forEach(function(module) {
 
 // Loading global options (files paths)
 var conf = VarStream.parse(Fs.readFileSync(__dirname+'/config.dat'))
-  , server;
+  , server
+  , prod = gulp.env.prod;
 
-// Starting the dev static server
-if(!gulp.env.prod) {
+if(!prod) {
+  // Finding the server IP
   conf.ip = '127.0.0.1';
 
   if(gulp.env.net) {
@@ -36,72 +37,71 @@ if(!gulp.env.prod) {
       }
     }
   }
-
-  var app = express();
-  app.use(express.query())
-    .use(express.bodyParser())
-    .use(express.static(Path.resolve(__dirname, conf.build.root)))
-    .listen(8080, function() {
-      console.log('Listening on %d', 35729);
-    });
-  server = tinylr();
-  server.listen(35729);
-
-  conf.baseURL = 'http://'+conf.ip+':8080';
 }
 
 // Fonts
-gulp.task('build_fonts', function() {
+gulp.task('build_fonts', function(cb) {
   gulp.src(conf.src.icons + '/**/*.svg', {buffer: conf.buffer})
     .pipe(gIconfont({
       'fontName': 'iconsfont',
       'appendCodepoints': true
     }))
-    .pipe(gulp.dest(conf.build.fonts));
+    .pipe(gulp.dest(conf.build.fonts))
+    .once('end', cb);
 });
 
 // Images
-gulp.task('build_images', function() {
+gulp.task('build_images', function(cb) {
+  var n = 0;
+  function end() { ++n==2 && cb(); }
   gulp.src(conf.src.images + '/**/*.svg') // , {buffer: conf.buffer} no streams
-    .pipe(gIf(!gulp.env.prod, gWatch()))
-    .pipe(gIf(gulp.env.prod, gSvgmin()))
-    .pipe(gIf(!gulp.env.prod, gLivereload(server)))
-    .pipe(gulp.dest(conf.build.images));
+    .pipe(gIf(!prod, gWatch()))
+    .pipe(gIf(prod, gSvgmin()))
+    .pipe(gIf(!prod, gLivereload(server)))
+    .pipe(gulp.dest(conf.build.images))
+    .once('end', end);
 
   gulp.src(conf.src.images + '/**/*.{png,jpg,jpeg,gif}') // , {buffer: conf.buffer} no streams
-    .pipe(gIf(!gulp.env.prod, gWatch()))
-    .pipe(gIf(gulp.env.prod, gImagemin()))
-    .pipe(gIf(!gulp.env.prod, gLivereload(server)))
-    .pipe(gulp.dest(conf.build.images));
+    .pipe(gIf(!prod, gWatch()))
+    .pipe(gIf(prod, gImagemin()))
+    .pipe(gIf(!prod, gLivereload(server)))
+    .pipe(gulp.dest(conf.build.images))
+    .once('end', end);
 });
 
 // CSS
-gulp.task('build_styles', function() {
+gulp.task('build_styles', function(cb) {
   gulp.src(conf.src.less + '/main.less') // , {buffer: conf.buffer} no streams
     .pipe(gLess())
-    .pipe(gIf(gulp.env.prod, gMinifyCss()))
-    .pipe(gIf(!gulp.env.prod, gLivereload(server)))
-    .pipe(gulp.dest(conf.build.css));
+    .pipe(gIf(prod, gMinifyCss()))
+    .pipe(gIf(!prod, gLivereload(server)))
+    .pipe(gulp.dest(conf.build.css))
+    .once('end', cb);
 });
 
 // JavaScript
-gulp.task('build_js', function() {
+gulp.task('build_js', function(cb) {
+  var n = 0;
+  function end() { ++n==3 && cb(); }
   gulp.src(conf.src.js + '/**/*.js', {buffer: conf.buffer})
-    .pipe(gJshint());
+    .pipe(gJshint())
+    .once('end', end);
 
-  gulp.src(conf.src.js + '/frontend.js', {buffer: conf.buffer && !gulp.env.prod}) // no streams in prod
+  gulp.src(conf.src.js + '/frontend.js', {buffer: conf.buffer && !prod}) // no streams in prod
+    .once('end', end) // gBrowserify never triggers 'end' ...
     .pipe(gBrowserify())
-    .pipe(gIf(gulp.env.prod, gUglify()))
+    .pipe(gIf(prod, gUglify()))
     .pipe(gConcat('script.js'))
-    .pipe(gIf(!gulp.env.prod, gLivereload(server)))
+    .pipe(gIf(!prod, gLivereload(server)))
     .pipe(gulp.dest(conf.build.frontjs));
 
   gulp.src(conf.src.js + '/frontend/vendors/**/*.js')
-    .pipe(gulp.dest(conf.build.frontjs + '/vendors'));
+    .pipe(gulp.dest(conf.build.frontjs + '/vendors'))
+    .once('end', end);
 });
 
 // HTML
-gulp.task('build_html', function() {
+gulp.task('build_html', function(cb) {
   var nunjucks = Nunjucks
     , tree = {}
     , markedFiles = []
@@ -126,7 +126,7 @@ gulp.task('build_html', function() {
       markedFiles.forEach(function(file) {
         var nunjucksOptions = {
           env: conf.build.root,
-          prod: gulp.env.prod,
+          prod: prod,
           tree: tree,
           conf: conf,
           metadata: file.metas,
@@ -140,21 +140,37 @@ gulp.task('build_html', function() {
         // Save it.
         dest.write(file);
       });
-      if(!gulp.env.prod) {
-        require('open')(conf.baseURL+'/index.html');
-      }
       dest.end();
+      cb();
     })
     .on('data', function(file) {
       markedFiles.push(file);
     }).resume();
 });
 
-// The build task
-gulp.task('build', function() {
-  rimraf.sync(conf.build.root);
-  gulp.run('build_fonts', 'build_images', 'build_styles', 'build_js', 'build_html');
-  if(!gulp.env.prod) {
+// The clean task
+gulp.task('clean', function(cb) {
+  rimraf(conf.build.root, cb);
+});
+
+// The build task'clean', 
+gulp.task('build', ['build_fonts', 'build_images', 'build_styles',
+  'build_js', 'build_html'], function(cb) {
+  if(!prod) {
+    // Starting the dev static server
+
+    var app = express();
+    app.use(express.query())
+      .use(express.bodyParser())
+      .use(express.static(Path.resolve(__dirname, conf.build.root)))
+      .listen(8080, function() {
+        console.log('Listening on %d', 35729);
+      });
+    server = tinylr();
+    server.listen(35729);
+
+    conf.baseURL = 'http://'+conf.ip+':8080';
+    require('open')(conf.baseURL+'/index.html');
     gulp.watch([conf.src.icons + '/**/*.svg'], function(event) {
       gulp.run('build_fonts');
     });
@@ -176,11 +192,13 @@ gulp.task('build', function() {
     ], function(event) {
       gulp.run('build_html');
     });
+
   }
+  cb();
 });
 
 // Publish task
-gulp.task('ghpages', function() {
+gulp.task('ghpages', function(cb) {
   var exec = require('child_process').exec
     , execOptions = {
       cwd: __dirname
@@ -191,31 +209,33 @@ gulp.task('ghpages', function() {
     var ignore = ['.git', 'www', 'node_modules'];
     Fs.readdirSync(__dirname).forEach(function(file) {
       if(-1 === ignore.indexOf(file)) {
-        //rimraf.sync(__dirname+'/'+file);
-        console.log('delete '+file);
-      } else {
-        console.log('keep '+file);
+        rimraf.sync(__dirname+'/'+file);
       }
-    }); return;
+    });
     Fs.readdirSync(__dirname+'/www').forEach(function(file) {
-      fs.renameSync(__dirname+'/www/'+file, __dirname+'/'+file);
+      Fs.renameSync(__dirname+'/www/'+file, __dirname+'/'+file);
     });
     Fs.rmdirSync(__dirname+'/www');
+    // Add the domain name
+    Fs.writeFileSync(__dirname + '/CNAME', conf.domain);
+    // Add a new ignore file
+    ignore.push('.gitignore');
+    Fs.writeFileSync(__dirname + '/.gitignore', ignore.join('\n'));
     // Commit files
-    exec('git add -A && git commit -m "Build '+(new Date())+'"', execOptions, function() {
+    exec('git add . && git commit -m "Build '+(new Date())+'"', execOptions, function() {
       
       // Pushing commit
       exec('git push -f origin gh-pages &&  git checkout master', execOptions, function() {
-
+        cb();
       });
     });
   });
 });
 
 // Publish task
-gulp.task('publish', function() {
-  gulp.env.prod = true;
-  gulp.run('build','ghpages');
+gulp.task('publish', ['build'], function() {
+  prod = true;
+  gulp.run('ghpages');
 });
 
 // The default task
