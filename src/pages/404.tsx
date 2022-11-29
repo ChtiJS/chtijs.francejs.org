7fcfd0443c90debbbb8b0cd6423517a591429f62
unknown <hugo.houdain2@gmail.com>
2022-11-29 16:52:35 +0100

    feat(styles): Changed 404 styles

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -7,8 +7,21 @@ const Page = () => (
   <Layout title="Page non-trouvée">
     <ContentBlock>
       <Heading1>Ooops!</Heading1>
-      <Paragraph>La page que vous recherchez n’existe pas ou plus.</Paragraph>
+      <p><img src={'/images/404.png'} alt={`404.png`} />
+        <Paragraph>La page que vous recherchez n’existe pas ou plus.</Paragraph></p>
+
     </ContentBlock>
+    <style jsx>{`
+        img {
+          height: 19rem;
+          width: 6rem;
+          border-radius: 15px;
+        }
+        p {
+          text-align: center;
+          line-height: 100px;
+        }
+      `}</style>
   </Layout>
 );
 
