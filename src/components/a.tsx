04f539b2cb1dbc43c24f1b42118225f4999674c0
Nicolas Froidure <nicolas.froidure@gmail.com>
2023-12-05 16:04:33 +0100

    feat(css): use CSS modules instead

--- src/components/a.tsx
+++ src/components/a.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { publicRuntimeConfig } from '../utils/config';
+import styles from './a.module.scss';
 import type { LinkProps } from 'next/link';
 
 const Anchor = ({
@@ -43,52 +43,24 @@ const Anchor = ({
       }}
     >
       <a
-        className={`root${className ? ' ' + className : ''}${
-          icon ? ` ${iconPosition}` : ''
+        className={`${styles.root}${className ? ' ' + className : ''}${
+          icon
+            ? ` ${styles.withIcon} ${
+                iconPosition === 'first' ? styles.first : styles.last
+              }`
+            : ''
         }`}
         {...props}
         target={href.startsWith('http') ? '_blank' : '_self'}
       >
-        {icon ? <span className="icon" /> : null}
+        {icon ? <span className={styles.icon} /> : null}
         {isURLLink
-          ? [href.replace(/(http|ftp)s?:\/\//, '').slice(0, 15) + '…' + href.slice(-5)]
+          ? [
+              href.replace(/(http|ftp)s?:\/\//, '').slice(0, 15) +
+                '…' +
+                href.slice(-5),
+            ]
           : children}
-        <style jsx>{`
-          a,
-          a:visited {
-            color: #c1a008;
-            transition: color 0.1s linear;
-            text-decoration: none;
-          }
-          a:hover,
-          a:focus {
-            color: var(--primary);
-          }
-          a.first,
-          a.last {
-            display: inline-flex;
-            flex-direction: row;
-            gap: calc(var(--gutter) / 4);
-          }
-          a.first span.icon,
-          a.last span.icon {
-            display: flex;
-            height: var(--vRythm);
-            width: calc(var(--vRythm) * 0.55);
-            background: var(--primary);
-            mask-repeat: no-repeat;
-            mask-size: calc(var(--vRythm) * 0.55);
-            -webkit-mask-size: calc(var(--vRythm) * 0.55);
-            mask-position: left bottom;
-            mask-image: url('${publicRuntimeConfig.basePath}/images/icons/arrow-left.svg');
-          }
-          a.last {
-            flex-direction: row-reverse;
-          }
-          a.last span.icon {
-            mask-image: url('${publicRuntimeConfig.basePath}/images/icons/arrow-right.svg');
-          }
-        `}</style>
       </a>
     </Link>
   );
