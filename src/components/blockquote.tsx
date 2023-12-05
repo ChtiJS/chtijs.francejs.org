c53de1b5324aff674e2949e43534aebfa7d58c69
amandine <amandine17021702@gmail.com>
2023-12-05 16:52:15 +0100

    refactor(tootList):change styles on tootList and blockquote

--- src/components/blockquote.tsx
+++ src/components/blockquote.tsx
@@ -1,21 +1,13 @@
+import styles from './blockquote.module.scss';
+
 const Blockquote = ({
   children,
   ...props
 }: {
   children: React.ReactNode;
 } & React.BlockquoteHTMLAttributes<HTMLElement>) => (
-  <blockquote className="root" {...props}>
+  <blockquote className={styles.root} {...props}>
     {children}
-    <style jsx>{`
-      .root {
-        font-family: var(--writingFont);
-        font-size: var(--greatFontSize);
-        line-height: var(--greatLineHeight);
-        margin: 0 0 var(--vRythm) 0;
-        padding: 0 0 0 var(--gutter);
-        border-left: var(--border) solid var(--green2);
-      }
-    `}</style>
   </blockquote>
 );
 
--- src/components/tootList.tsx
+++ src/components/tootList.tsx
@@ -1,4 +1,5 @@
 import Heading2 from './h2';
+import styles from './tootList.module.scss';
 import { renderMarkdown } from '../utils/markdown';
 import Paragraph from './p';
 import Anchor from './a';
@@ -12,11 +13,11 @@ export type Toots = {
 }[];
 
 const TootList = ({ toots }: { toots: Toots }) => (
-  <aside className="root">
+  <aside className={styles.root}>
         <Heading2>Derniers messages Mastodon&nbsp;:</Heading2>
-        <div className="tootList">
+        <div className={styles.tootList}>
           {toots.map((toot) => (
-            <div key={toot.id} className="toot">
+            <div key={toot.id} className={styles.toot}>
               {renderMarkdown({ index: 0 }, toot.text)}
               <Paragraph>
                 Publié le{" "}
@@ -27,35 +28,6 @@ const TootList = ({ toots }: { toots: Toots }) => (
             </div>
           ))}
         </div>
-    <style jsx>{`
-      .root {
-        margin: 0 0 var(--vRythm) 0;
-      }
-      .toot :global(a) {
-        color: #205370;
-      }
-      .toot :global(p) {
-        margin-left: 2%;
-      }
-      .toot :global(a):hover {
-        color: #14171a;
-      }
-
-      .tootList {
-        margin-top: var(--vRythm);
-        display: flex;
-        flex-direction: row;
-        gap: var(--gutter);
-      }
-      .toot {
-        border-style: solid;
-        border-color: var(--grey);
-        border-size: var(--borderSize);
-        border-radius: var(--borderRadius);
-        width: var(--block);
-        padding: calc(var(--vRythm) / 2) calc(var(--gutter) / 2);
-      }
-    `}</style>
   </aside>
 );
 
