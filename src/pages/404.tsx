5f452a2adc8e3af73d780de5c50c7e0dcbf7b953
amandine <amandine17021702@gmail.com>
2023-12-12 11:31:02 +0100

    refactor(404):change styles on 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,27 +2,17 @@ import Layout from '../layouts/main';
 import ContentBlock from '../components/contentBlock';
 import Heading1 from '../components/h1';
 import Paragraph from '../components/p';
+import styles from './404.module.scss';
 
 const Page = () => (
   <Layout title="Page non-trouvée">
     <ContentBlock>
       <Heading1>Ooops!</Heading1>
-      <p>
-        <img src={'/images/404.png'} alt={`404.png`} />
+      <p className={styles.p} >
+        <img className={styles.img} src={'/images/404.png'} alt={`404.png`} />
         <Paragraph>La page que vous recherchez n’existe pas ou plus.</Paragraph>
       </p>
     </ContentBlock>
-    <style jsx>{`
-      img {
-        height: 19rem;
-        width: 6rem;
-        border-radius: 15px;
-      }
-      p {
-        text-align: center;
-        line-height: 100px;
-      }
-    `}</style>
   </Layout>
 );
 
