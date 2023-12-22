14e315528f9ab470b7442a4c2e9d1835ae9ab9d0
nfroidure <nicolas.froidure@gmail.com>
2023-12-22 10:14:28 +0000

    Deploying to gh-pages from @ ChtiJS/chtijs.francejs.org@6996e2e42cddade33e56bbdf18a5faac6a1ce507 🚀

---
+++ _next/static/cWrIx5TJK2cNOVumkLxZs/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[id]","\u002Fconferences\u002F[id]","\u002Fconferences\u002Fpages\u002F[page]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- _next/static/jidruHoudrMY6kquEEJhK/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002F[id]","\u002Fconferences","\u002Fconferences\u002F[id]","\u002Fconferences\u002Fpages\u002F[page]","\u002Fcredits","\u002Fmembres","\u002Fplanete"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
